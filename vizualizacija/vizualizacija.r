8f59e4b02ed1e310210fb8643971bd0c92ff43ae
Lonja Tavcar <lonja10@gmail.com>
2016-02-28 09:59:33 +0100

    Dodala sem zemljevid.

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -3,7 +3,24 @@
 # Uvozimo zemljevid.
 obcine <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip", "OB/OB", encoding = "Windows-1250")
 
-zem <- ggplot() + geom_polygon(data = muzejiinrazstavisca, aes(x=long, y=lat, group=group, fill=x2014), color= "red")+scale_fill_gradient(low="#3F7F3F", high="#00FF00")+guides(fill=guide_colorbar(title = "Muzeji 2014"))
+uvozi.muzeje <- function(){
+  return(read.csv("podatki/muzejiinrazstavisca.csv", dec = ".", na.strings ="-",
+                  header = FALSE, col.names = leta, fileEncoding = "Windows-1250"))
+}
+
+muzeji <- uvozi.muzeje() %>% melt(variable.name = "Leto", value.name = "Stevilo")
+levels(muzeji$Obcina) <- levels(muzeji$Obcina) %>% {gsub(" - ", "-", .)} %>%
+  strapplyc("^([^/]+)") %>% unlist()
+muzeji$Leto <- muzeji$Leto %>% as.character() %>% strapplyc("([0-9]+)") %>%
+  unlist() %>% as.numeric()
+
+ob <- pretvori.zemljevid(obcine)
+zem <- ggplot() + geom_polygon(data = muzeji %>% filter(Leto == 2014) %>%
+                                 right_join(ob, by = c("Obcina" = "OB_UIME")),
+                               aes(x=long, y=lat, group=group, fill=Stevilo),
+                               color= "red") +
+  scale_fill_gradient(low="#3F7F3F", high="#00FF00") +
+  guides(fill=guide_colorbar(title = "Muzeji 2014"))
 print(zem)
 # Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
 #druzine <- preuredi(druzine, zemljevid, "OB_UIME", c("Ankaran", "Mirna"))
