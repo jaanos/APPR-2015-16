d9554ce8aec90188654351ad4123cf9945b902b9
Lonja Tavcar <lonja10@gmail.com>
2016-03-23 18:43:22 +0100

    graf pri končni analizi

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,45 +1,13 @@
 # 4. faza: Analiza podatkov
 
 
+obiskovalci.tidy <- data.frame(leto = rownames(OK.obiskovalci),
+                               OK.obiskovalci) %>%
+  melt(variable.name = "regija", value.name = "stevilo")
 
+graf <- ggplot(obiskovalci.tidy, aes(x = leto, y = stevilo,
+                             group = regija, color = regija)) +
+  geom_line() + geom_point() + ggtitle("Obiskovalci") +
+  xlab("Leta") + ylab("Skupaj")
 
-max_y <- max(OK.obiskovalci)
-
-
-plot_colors <- c("blue","red","darkgreen","black","gray20","gold","darkmagenta","darkorange","brown","deeppink","chocolate","cyan")
-
-
-plot(OK.obiskovalci$Pomurska, type="o", col=plot_colors[1], 
-     ylim=c(0,max_y), axes=FALSE, ann=FALSE)
-
-
-axis(1, at=1:6, lab=c("2007", "2008", "2009", "2010", "2013", "2014"))
-
-
-axis(2, las=1, at=1000*0:max_y)
-
-
-#box()
-
-lines(OK.obiskovalci$Podravska, type="o", col=plot_colors[2])
-
-
-lines(OK.obiskovalci$Koroška, type="o", col=plot_colors[3])
-lines(OK.obiskovalci$Savinjska, type="o", col=plot_colors[4])
-lines(OK.obiskovalci$Zasavska, type="o", col=plot_colors[5])
-lines(OK.obiskovalci$Spodnjeposavska, type = "o", col=plot_colors[6])
-lines(OK.obiskovalci$Jugovzhodna, type ="o", col=plot_colors[7])
-lines(OK.obiskovalci$Osrednjeslovenska, type ="o", col=plot_colors[8])
-lines(OK.obiskovalci$Gorenjska, type = "o", col=plot_colors[9])
-lines(OK.obiskovalci$Notranjsko.kraška,type="o", col=plot_colors[10])
-lines(OK.obiskovalci$Goriška,type = "o", col=plot_colors[11])
-lines(OK.obiskovalci$Obalno.kraška, type="o", col=plot_colors[12])
-
-title(main="Obiskovalci", col.main="red", font.main=4)
-
-
-title(xlab= "Leta", col.lab=rgb(0,0.5,0))
-title(ylab= "Skupaj", col.lab=rgb(0,0.5,0))
-
-
-#legend("right",names(OK.obiskovalci), cex=0.8, col=plot_colors, pch=20:27, lty=1:20, xjust=0.5);
+plot(graf)
--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -7,18 +7,17 @@ leta <- c("Obcina","2007", "2008", "2009","2010","2011","2012","2013","2014")
 
 #Uvoz tabele s številom muzejev:
   
-#uvozi.stevilomuzejev <- function(){return(read.csv2("podatki/stmuzejevnapreb.csv", header = FALSE, na.strings = "...", row.names = 1, col.names = regije))
- # }
+uvozi.stevilomuzejev <- function(){return(read.csv2("podatki/stmuzejevnapreb.csv", header = FALSE, na.strings = "...", row.names = 1, col.names = regije))
+  }
 
 #Uvoz procenta občasnih razstav:
   
-#uvozi.obcasne <- function(){return(read.csv2("podatki/obcasnerazsprocent.csv", header = FALSE, na.strings = "...", row.names = 1, col.names = regije))}
+uvozi.obcasne <- function(){return(read.csv2("podatki/obcasnerazsprocent.csv", header = FALSE, na.strings = "...", row.names = 1, col.names = regije))}
 
 #Uvoz povprečnega števila obiskovalcev:
   
-#uvozi.obiskovalce <- function(){return(read.csv2("podatki/stobiskovalcev.csv", header = FALSE, na.strings = "...", row.names = 1, col.names = regije))
- # }
-#uvozi.muzeje <- function(){return(read.csv("podatki/muzejiinrazstavisca.csv", dec = ".", na.strings ="-", header = FALSE, row.names=1, col.names = leta))}
+uvozi.obiskovalce <- function(){return(read.csv2("podatki/stobiskovalcev.csv", header = FALSE, na.strings = "...", row.names = 1, col.names = regije)) }
+uvozi.muzeje <- function(){return(read.csv("podatki/muzejiinrazstavisca.csv", dec = ".", na.strings ="-", header = FALSE, row.names=1, col.names = leta))}
 #Zapišemo v tabelco:
   
 stevilomuzejev <- uvozi.stevilomuzejev()
@@ -33,20 +32,20 @@ obiskovalci <-uvozi.obiskovalce()
 OK.vrstice3 <- apply(obiskovalci, 1, function(x){!any(is.na(x))})
 OK.obiskovalci <- obiskovalci[OK.vrstice3, ]
 
-muzeji<-uvozi.muzeje()
-OK.vrstice4 <- apply(muzejiinrazstavisca, 1, function(x){!any(is.na(x))})
-ok.muzeji <- muzeji[OK.vrstice4, ]
+muzeji1<-uvozi.muzeje()
+OK.vrstice4 <- apply(muzeji1, 1, function(x){!any(is.na(x))})
+ok.muzeji <- muzeji1[OK.vrstice4, ]
 
 
 
 stevilomuzejev <- read.csv2("podatki/stmuzejevnapreb.csv", dec =".", header = FALSE, na.strings = "...", col.names = regije)%>%melt(id.vars = "Leto", variable.name = "Regija", value.name = "Stevilo.muzejev")
 
-obcasnerazstave <- read.csv2("podatki/obcasnerazsprocent.csv", dec = ".", header = FALSE, na.strings = "...", col.names = regije)%>%melt(id.vars = "Leto", variable.name = "Regija", value.name = "Obcasne.razstave")
+#obcasnerazstave <- read.csv2("podatki/obcasnerazsprocent.csv", dec = ".", header = FALSE, na.strings = "...", col.names = regije)%>%melt(id.vars = "Leto", variable.name = "Regija", value.name = "Obcasne.razstave")
 
 obiskmuzejev <- read.csv2("podatki/stobiskovalcev.csv", dec = ".", header = FALSE, na.strings = "...", col.names = regije)%>%melt(id.vars = "Leto", variable.name = "Regija", value.name = "Obisk.muzejev")
 
 
-skupaj <- stevilomuzejev %>% full_join(obcasnerazstave) %>% full_join(obiskmuzejev)
+skupaj <- stevilomuzejev %>% full_join(obiskmuzejev) 
 
 
 
