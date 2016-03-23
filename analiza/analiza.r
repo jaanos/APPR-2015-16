7564fc5e2b579867d5e8bccbf13e5f95e832acb4
Lonja Tavcar <lonja10@gmail.com>
2016-03-23 19:01:12 +0100

    šeen graf

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -11,3 +11,15 @@ graf <- ggplot(obiskovalci.tidy, aes(x = leto, y = stevilo,
   xlab("Leta") + ylab("Skupaj")
 
 plot(graf)
+
+obcasne.tidy <- data.frame(leto = rownames(OK.obcasne),
+                               OK.obcasne) %>%
+  melt(variable.name = "regija", value.name = "stevilo")
+
+graf2 <- ggplot(obcasne.tidy, aes(x = leto, y = stevilo,
+                                     group = regija, color = regija)) +
+  geom_line() + geom_point() + ggtitle("Občasne razstave") +
+  xlab("Leta") + ylab("Skupaj")
+
+plot(graf2)
+
